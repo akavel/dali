350dbfb181ae3e052875a65081cb43b80173ff61
Mateusz Czapliński <czapkofan@gmail.com>
2019-03-06 03:22:49 +0100

    fix to compile

--- src/dali.nim
+++ src/dali.nim
@@ -1,4 +1,5 @@
 {.experimental: "codeReordering".}
+import strutils
 import std/sha1
 
 proc sample_dex*(tail: string): string =
@@ -14,7 +15,8 @@ proc sample_dex*(tail: string): string =
   header.write(0x28, 0x12345678)
   # SHA1 hash
   # TODO: should allow hashing a "stream", to not allocate new string...
-  header.write(0x10, secureHash(header.substr(0x20) & tail))
+  let sha1 = secureHash(header.substr(0x20) & tail)
+  header.write(0x10, parseHexStr($sha1))
   # Adler checksum
   header.write(0x08, adler32(header.substr(0x10) & tail))
   return header & tail
@@ -22,23 +24,23 @@ proc sample_dex*(tail: string): string =
 proc write(s: var string, pos: int, what: string) =
   if pos + what.len > s.len:
     setLen(s, pos + what.len)
-  copyMem(addr(s[pos]), what, what.len)
+  copyMem(addr(s[pos]), cstring(what), what.len)
 
 proc write(s: var string, pos: int, what: uint32) =
   # Little-endian
   let buf = [
-    chr(v and 0xff),
-    chr(v shr 8 and 0xff),
-    chr(v shr 16 and 0xff),
-    chr(v shr 24 and 0xff)]
-  s.write(pos, buf)
+    chr(what and 0xff),
+    chr(what shr 8 and 0xff),
+    chr(what shr 16 and 0xff),
+    chr(what shr 24 and 0xff)]
+  s.write(pos, $buf)
 
 proc adler32(s: string): uint32 =
   var a: uint32 = 1
   var b: uint32 = 0
   const MOD_ADLER = 65521
   for c in s:
-    a = (a + c.uint32) % MOD_ADLER
-    b = (b + a) % MOD_ADLER
+    a = (a + c.uint32) mod MOD_ADLER
+    b = (b + a) mod MOD_ADLER
   result = (b shl 16) or a
 
--- tests/tdex.nim
+++ tests/tdex.nim
@@ -1,10 +1,11 @@
 {.experimental: "codeReordering".}
 import unittest
+import strutils
 import dali
 
 test "hello world.apk":
   # Based on: https://github.com/corkami/pics/blob/master/binary/DalvikEXecutable.pdf
-  want = strip_space"""
+  let want = strip_space"""
 .d .e .x 0A .0 .3 .5 00  6F 53 89 BC 1E 79 B2 4F
 1F 9C 09 66 15 23 2D 3B  56 65 32 C3 B5 81 B4 5A
 70 02 00 00 70 00 00 00  78 56 34 12 00 00 00 00
@@ -44,24 +45,60 @@ EC 00 00 00 01 20 00 00  01 00 00 00 0C 01 00 00
 01 10 00 00 02 00 00 00  2C 01 00 00 02 20 00 00
 0C 00 00 00 3A 01 00 00  00 20 00 00 01 00 00 00
 D1 01 00 00 00 10 00 00  01 00 00 00 DC 01 00 00
-"""
-  let tail = want.substr(0x2C * 2).parseHexStr
-  let have = sample_dex(tail).hexify
-  check have == want
+""".dehexify
+  let tail = want.substr(0x2C)
+  let have = sample_dex(tail)
+  # check have.hexify == want.hexify
+  # check have.toHex == want.toHex
+  check have.dumpHex == want.dumpHex
 
 proc strip_space(s: string): string =
   return s.multiReplace(("\n", ""), (" ", ""))
 
+const HexChars = "0123456789ABCDEF"
+
+func printable(c: char): bool =
+  let n = ord(c)
+  return 0x21 <= n and n <= 0x7E
+
 proc hexify(s: string): string =
   # Based on strutils.toHex
-  const HexChars = "0123456789ABCDEF"
   result = newString(s.len * 2)
   for pos, c in s:
-    let n = ord(c)
-    if 0x21 <= n and n <= 0x7E:
+    if printable(c):
       result[pos * 2] = '.'
       result[pos * 2 + 1] = c
     else:
+      let n = ord(c)
       result[pos * 2] = HexChars[n shr 4]
       result[pos * 2 + 1] = HexChars[n and 0x0F]
+
+proc dehexify(s: string): string =
+  result = newString(s.len div 2)
+  for i in 0 ..< s.len div 2:
+    let chunk = s.substr(2 * i, 2 * i + 1)
+    if chunk[0] == '.':
+      result[i] = chunk[1]
+    else:
+      result[i] = parseHexStr(chunk)[0]
+
+proc dumpHex(s: string): string =
+  if s.len == 0: return ""
+  let nlines = (s.len + 15) div 16
+  const
+    left = 3*8 + 2 + 3*8 + 2
+    right = 16
+    line = left+right+1
+  result = ' '.repeat(nlines*line)
+  for i, ch in s:
+    let
+      y = i div 16
+      xr = i mod 16
+      xl = if xr < 8: 3*xr else: 3*xr + 1
+      n = ord(ch)
+    result[y*line + xl] = HexChars[n shr 4]
+    result[y*line + xl + 1] = HexChars[n shr 4]
+    result[y*line + left + xr - 1] = if printable(ch): ch else: '.'
+    if xr == 0:
+      result[y*line + left + right - 1] = '\n'
 
