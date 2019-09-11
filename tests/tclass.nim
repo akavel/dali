5238747e0c700b592eb92bc1f0df508c2249627d
Mateusz Czapliński <czapkofan@gmail.com>
2019-09-11 22:15:04 +0200

    tests/tclass: add test for hello_android.apk

--- tests/tclass.nim
+++ tests/tclass.nim
@@ -297,3 +297,70 @@ test "bugsnag.apk":
     )
   )
 
+test "hello_android.apk":
+  let c =
+    jclass com.android.hello.HelloAndroid {.public.} of Activity:
+      proc `<init>`() {.public, constructor, regs:1, ins:1, outs:1.} =
+        invoke_direct(0, jproto Activity.`<init>`())
+        return_void()
+      proc onCreate(Bundle) {.public, regs:3, ins:2, outs:2.} =
+        invoke_super(1, 2, jproto Activity.onCreate(Bundle))
+        const_high16(0, 0x7f03)
+        invoke_virtual(1, 0, jproto HelloAndroid.setContentView(int))
+        return_void()
+  checkpoint c.repr
+  check c.equals ClassDef(
+    class: "Lcom/android/hello/HelloAndroid;",
+    access: {Public},
+    superclass: SomeType("Landroid/app/Activity;"),
+    class_data: ClassData(
+      direct_methods: @[
+        EncodedMethod(
+          m: Method(
+            class: "Lcom/android/hello/HelloAndroid;",
+            name: "<init>",
+            prototype: Prototype(ret: "V", params: @[]),
+          ),
+          access: {Public, Constructor},
+          code: SomeCode(Code(
+            registers: 1,
+            ins: 1,
+            outs: 1,
+            instrs: @[
+              invoke_direct(0, Method(class: "Landroid/app/Activity;", name: "<init>",
+                prototype: Prototype(ret: "V", params: @[]))),
+              return_void(),
+            ],
+          )),
+        ),
+      ],
+      virtual_methods: @[
+        EncodedMethod(
+          m: Method(
+            class: "Lcom/android/hello/HelloAndroid;",
+            name: "onCreate",
+            prototype: Prototype(
+              ret: "V",
+              params: @["Landroid/os/Bundle;"],
+            ),
+          ),
+          access: {Public},
+          code: SomeCode(Code(
+            registers: 3,
+            ins: 2,
+            outs: 2,
+            instrs: @[
+              invoke_super(1, 2, Method(class: "Landroid/app/Activity;", name: "onCreate",
+                prototype: Prototype(ret: "V", params: @["Landroid/os/Bundle;"]))),
+              const_high16(0, 0x7f03),
+              invoke_virtual(1, 0, Method(class: "Lcom/android/hello/HelloAndroid;", name: "setContentView",
+                prototype: Prototype(ret: "V", params: @["I"]))),
+              return_void(),
+            ],
+          )),
+        ),
+      ],
+    )
+  )
+
+
