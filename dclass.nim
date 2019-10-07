a4bdd282e51e67757b9882f1b232a4f021d8e297
Mateusz Czapliński <czapkofan@gmail.com>
2019-10-07 22:26:25 +0200

    dclass: proc stringFromField

--- dclass.nim
+++ dclass.nim
@@ -22,38 +22,60 @@ let
   Context = "Landroid/content/Context;"
   View = "Landroid/view/View;"
   CharSequence = "Ljava/lang/CharSequence;"
+  Long = "Ljava/lang/Long;"
 
-expandMacros:
-  classes_dex:
-    dclass com.akavel.hello2.HelloActivity {.public.} of Activity:
-      proc `<clinit>`() {.static, constructor, regs:2, ins:0, outs:1.} =
-        # System.loadLibrary("hello-mello")
-        const_string(0, "hello-mello")
-        invoke_static(0, jproto System.loadLibrary(String))
-        return_void()
-      proc `<init>`() {.public, constructor, regs:1, ins:1, outs:1.} =
-        invoke_direct(0, jproto Activity.`<init>`())
-        return_void()
-      proc onCreate(Bundle) {.public, regs:4, ins:2, outs:2.} =
-        # ins: this, arg0
-        # super.onCreate(arg0)
-        invoke_super(2, 3, jproto Activity.onCreate(Bundle))
-        # v0 = new TextView(this)
-        new_instance(0, TextView)
-        invoke_direct(0, 2, jproto TextView.`<init>`(Context))
-        # v1 = this.stringFromJNI()
-        #  NOTE: failure to call a Native function should result in
-        #  java.lang.UnsatisfiedLinkError exception
-        invoke_virtual(2, jproto HelloActivity.stringFromJNI() -> String)
-        move_result_object(1)
-        # v0.setText(v1)
-        invoke_virtual(0, 1, jproto TextView.setText(CharSequence))
-        # this.setContentView(v0)
-        invoke_virtual(2, 0, jproto HelloActivity.setContentView(View))
-        # return
-        return_void()
-      proc stringFromJNI(): jstring {.public, native.} =
-        return jenv.NewStringUTF(jenv, "Hello from Nim dclass :D")
+# TODO 1:
+#  1. this.nimSelf = (long)42
+#  2. v2/3 = this.nimSelf
+#  3. v1 = ltoa(v2/3)
+#  4. return v1
+# TODO 2:
+#  - same as above, but steps 2+3 are implemented in stringFromJNI()
+
+classes_dex:
+  dclass com.akavel.hello2.HelloActivity {.public.} of Activity:
+    proc `<clinit>`() {.static, constructor, regs:2, ins:0, outs:1.} =
+      # System.loadLibrary("hello-mello")
+      const_string(0, "hello-mello")
+      invoke_static(0, jproto System.loadLibrary(String))
+      return_void()
+    proc `<init>`() {.public, constructor, regs:1, ins:1, outs:1.} =
+      invoke_direct(0, jproto Activity.`<init>`())
+      return_void()
+    proc onCreate(Bundle) {.public, regs:4, ins:2, outs:2.} =
+      # ins: this, arg0
+      # super.onCreate(arg0)
+      invoke_super(2, 3, jproto Activity.onCreate(Bundle))
+      # v0 = new TextView(this)
+      new_instance(0, TextView)
+      invoke_direct(0, 2, jproto TextView.`<init>`(Context))
+      # v1 = this.stringFromJNI()
+      #  NOTE: failure to call a Native function should result in
+      #  java.lang.UnsatisfiedLinkError exception
+      invoke_virtual(2, jproto HelloActivity.stringFromJNI() -> String)
+      move_result_object(1)
+      # v0.setText(v1)
+      invoke_virtual(0, 1, jproto TextView.setText(CharSequence))
+      # this.setContentView(v0)
+      invoke_virtual(2, 0, jproto HelloActivity.setContentView(View))
+      # return
+      return_void()
+    proc stringFromJNI(): jstring {.public, native.} =
+      return jenv.NewStringUTF(jenv, "Hello from Nim dclass :D")
+
+    proc stringFromField(): jstring {.regs:4, ins:1, outs:3.} =
+      # this.nimSelf = (long)42
+      const_wide_16(0, 42'i16)
+      iput_wide(0, 3,
+        Field(class:HelloActivity, typ:"J", name:"nimSelf"))
+      # v1..2 = this.nimSelf
+      iget_wide(1, 3,
+        Field(class:HelloActivity, typ:"J", name:"nimSelf"))
+      # v0 = Long.toString(v1..2)
+      invoke_static(1, 2, jproto Long.toString(Long))
+      move_result_object(0)
+      # return v0
+      return_object(0)
 
 # dumpTree:
 #   var foo, bing: seq[int]
--- src/dali/instrs.nim
+++ src/dali/instrs.nim
@@ -4,14 +4,27 @@ import dali/types
 
 proc move_result_object*(reg: uint8): Instr =
   return newInstr(0x0c, RegXX(reg))
+
 proc return_void*(): Instr =
   return newInstr(0x0e, RawXX(0))
+proc return_object*(reg: uint8): Instr =
+  return newInstr(0x11, RawXX(reg))
+
 proc const_high16*(reg: uint8, highBits: uint16): Instr =
   return newInstr(0x15, RegXX(reg), RawXXXX(highBits))
+proc const_wide_16*(regs: uint8, v: int16): Instr =
+  return newInstr(0x16, RegXX(regs), RawXXXX(v.uint16))
 proc const_string*(reg: uint8, s: String): Instr =
   return newInstr(0x1a, RegXX(reg), StringXXXX(s))
+
 proc new_instance*(reg: uint8, t: Type): Instr =
   return newInstr(0x22, RegXX(reg), TypeXXXX(t))
+
+proc iget_wide*(regsA: uint4, regB: uint4, field: Field): Instr =
+  return newInstr(0x53, RegX(regB), RegX(regsA), FieldXXXX(field))
+proc iput_wide*(regsA: uint4, regB: uint4, field: Field): Instr =
+  return newInstr(0x5a, RegX(regB), RegX(regsA), FieldXXXX(field))
+
 proc sget_object*(reg: uint8, field: Field): Instr =
   return newInstr(0x62, RegXX(reg), FieldXXXX(field))
 
@@ -30,6 +43,8 @@ proc invoke_direct*(regC: uint4, regD: uint4, m: Method): Instr =
 
 proc invoke_static*(regC: uint4, m: Method): Instr =
   return newInvoke1(0x71, regC, m)
+proc invoke_static*(regC, regD: uint4, m: Method): Instr =
+  return newInvoke2(0x71, regC, regD, m)
 
 
 proc newInvoke1*(opcode: uint8, regC: uint4, m: Method): Instr =
