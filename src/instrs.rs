390cacb1d1a661a10f673d8a7e395135943b26f5
Mateusz Czapliński <czapkofan@gmail.com>
2024-11-01 20:48:49 +0100

    refactor: add macro instr!()

--- src/instrs.rs
+++ src/instrs.rs
@@ -1,31 +1,28 @@
 use crate::types::{Arg::*, *};
 use u4::u4;
 
-pub fn return_void() -> Instr {
-    Instr {
-        opcode: 0x0e,
-        args: vec![RawXX(0)],
+macro_rules! instr {
+    ($op:expr $(; $( $arg:expr ),* $(,)? )? ) => {
+        Instr {
+            opcode: $op,
+            args: vec![ $( $( $arg, )* )? ],
+        }
     }
+}
+
+pub fn return_void() -> Instr {
+    instr!(0x0e; RawXX(0))
 }
 
 pub fn const_high16(reg: u8, high_bits: u16) -> Instr {
-    Instr {
-        opcode: 0x15,
-        args: vec![RegXX(reg), RawXXXX(high_bits)],
-    }
+    instr!(0x15; RegXX(reg), RawXXXX(high_bits))
 }
 pub fn const_string(reg: u8, s: String) -> Instr {
-    Instr {
-        opcode: 0x1a,
-        args: vec![RegXX(reg), StringXXXX(s)],
-    }
+    instr!(0x1a; RegXX(reg), StringXXXX(s))
 }
 
 pub fn sget_object(reg: u8, field: Field) -> Instr {
-    Instr {
-        opcode: 0x62,
-        args: vec![RegXX(reg), FieldXXXX(field)],
-    }
+    instr!(0x62; RegXX(reg), FieldXXXX(field))
 }
 
 pub fn invoke_virtual2(reg_c: U4, reg_d: U4, m: Method) -> Instr {
@@ -37,30 +34,20 @@ pub fn invoke_super2(reg_c: U4, reg_d: U4, m: Method) -> Instr {
 }
 
 pub fn invoke_direct1(reg_c: U4, m: Method) -> Instr {
-    Instr {
-        opcode: 0x70,
-        args: vec![
-            RawX(u4!(1)),
-            RawX(u4!(0)),
-            MethodXXXX(m),
-            RawX(u4!(0)),
-            RegX(reg_c),
-            RawXX(0),
-        ],
-    }
+    instr!(0x70;
+        RawX(u4!(1)), RawX(u4!(0)),
+        MethodXXXX(m),
+        RawX(u4!(0)), RegX(reg_c),
+        RawXX(0),
+    )
 }
 
 // helper
 fn invoke2(opcode: u8, reg_c: U4, reg_d: U4, m: Method) -> Instr {
-    Instr {
-        opcode,
-        args: vec![
-            RawX(u4!(2)),
-            RawX(u4!(0)),
-            MethodXXXX(m),
-            RegX(reg_d),
-            RegX(reg_c),
-            RawXX(0),
-        ],
-    }
+    instr!(opcode;
+        RawX(u4!(2)), RawX(u4!(0)),
+        MethodXXXX(m),
+        RegX(reg_d), RegX(reg_c),
+        RawXX(0),
+    )
 }
