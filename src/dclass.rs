86fa6a3636ccd4967d6b08f748a1843edbc6180c
Mateusz Czapliński <czapkofan@gmail.com>
2025-06-11 23:17:06 +0200

    start adding support for instrs in dclass! macro

--- src/dclass.rs
+++ src/dclass.rs
@@ -3,7 +3,9 @@ macro_rules! dclass {
     (
         $($name:ident).+ impl $superclass:ident {
             #[$( $fnmod:ident $(($modarg:literal))? ),+]
-            fn <$fn:ident>()
+            fn <$fn:ident>() {
+                $( $instr:ident( $($iargs:tt)* ) )+
+            }
         }
     ) => {
         ClassDef {
@@ -11,7 +13,12 @@ macro_rules! dclass {
             superclass: Some($superclass.clone()),
             class_data: Some(ClassData {
                 direct_methods: vec![
-                    dclass!( [emethod [$($name).+] [<$fn>] [$($fnmod $(($modarg))?)+]] ),
+                    dclass!( [emethod
+                        [$($name).+]
+                        [<$fn>]
+                        [$( $fnmod $(($modarg))? )+]
+                        [$( $instr( $($iargs)* ) )+]
+                    ] ),
                 ],
                 ..Default::default()
             }),
@@ -19,23 +26,27 @@ macro_rules! dclass {
         }
     };
     // helpers for EncodedMethod building
-    ( [emethod [$($c:tt)*] [$($f:tt)*] [Public $($modn:tt)*]] ) => {
-        dclass!( [emethod [$($c)*] [$($f)*] [$($modn)*]] ).with_access(Access::Public)
+    ( [emethod [$($c:tt)*] [$($f:tt)*] [Public $($modn:tt)*] [$($i:tt)*]] ) => {
+        dclass!( [emethod [$($c)*] [$($f)*] [$($modn)*] [$($i)*]] ).with_access(Access::Public)
     };
-    ( [emethod [$($c:tt)*] [$($f:tt)*] [Constructor $($modn:tt)*]] ) => {
-        dclass!( [emethod [$($c)*] [$($f)*] [$($modn)*]] ).with_access(Access::Constructor)
+    ( [emethod [$($c:tt)*] [$($f:tt)*] [Constructor $($modn:tt)*] [$($i:tt)*]] ) => {
+        dclass!( [emethod [$($c)*] [$($f)*] [$($modn)*] [$($i)*]] ).with_access(Access::Constructor)
     };
-    ( [emethod [$($c:tt)*] [$($f:tt)*] [Regs($n:literal) $($modn:tt)*]] ) => {
-        dclass!( [emethod [$($c)*] [$($f)*] [$($modn)*]] ).with_registers($n as u16)
+    ( [emethod [$($c:tt)*] [$($f:tt)*] [Regs($n:literal) $($modn:tt)*] [$($i:tt)*]] ) => {
+        dclass!( [emethod [$($c)*] [$($f)*] [$($modn)*] [$($i)*]] ).with_registers($n as u16)
     };
-    ( [emethod [$($c:tt)*] [$($f:tt)*] [Ins($n:literal) $($modn:tt)*]] ) => {
-        dclass!( [emethod [$($c)*] [$($f)*] [$($modn)*]] ).with_ins($n as u16)
+    ( [emethod [$($c:tt)*] [$($f:tt)*] [Ins($n:literal) $($modn:tt)*] [$($i:tt)*]] ) => {
+        dclass!( [emethod [$($c)*] [$($f)*] [$($modn)*] [$($i)*]] ).with_ins($n as u16)
     };
-    ( [emethod [$($c:tt)*] [$($f:tt)*] [Outs($n:literal) $($modn:tt)*]] ) => {
-        dclass!( [emethod [$($c)*] [$($f)*] [$($modn)*]] ).with_outs($n as u16)
+    ( [emethod [$($c:tt)*] [$($f:tt)*] [Outs($n:literal) $($modn:tt)*] [$($i:tt)*]] ) => {
+        dclass!( [emethod [$($c)*] [$($f)*] [$($modn)*] [$($i)*]] ).with_outs($n as u16)
     };
     (
-        [emethod [$($class:ident).+] [<$fn:ident>] [] ]
+        [emethod
+            [$($class:ident).+]
+            [<$fn:ident>]
+            []
+            [$( $instr:ident( $($iargs:tt)* ) )+]]
     ) => {
         EncodedMethod {
             m: Method {
@@ -43,6 +54,12 @@ macro_rules! dclass {
                 name: "<".to_owned() + stringify!($fn) + ">",
                 ..Default::default() // FIXME
             },
+            code: Some(Code {
+                instrs: vec![
+                    $( $instr( $($iargs)* ) ),+
+                ],
+                ..Default::default()
+            }),
             ..Default::default()
         }
     };
@@ -68,7 +85,10 @@ mod tests {
         let c = dclass! {
             com.bugsnag.dexexample.BugsnagApp impl application {
                 #[Public, Constructor, Regs(1), Ins(1), Outs(1)]
-                fn <init>()
+                fn <init>() {
+                    //invoke_direct(0, @application.<init>())
+                    return_void()
+                }
             }
         };
         assert_eq!(c, ClassDef {
