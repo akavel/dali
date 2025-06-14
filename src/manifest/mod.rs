5ebbc1ebdf69675217b7cb6be670e0c9a520f21e
Mateusz Czapliński <czapkofan@gmail.com>
2025-06-14 13:13:41 +0200

    TDD manifest::compile() start

--- src/manifest/mod.rs
+++ src/manifest/mod.rs
@@ -1,1 +1,62 @@
-pub mod dump;
+use std::io;
+
+use android_manifest::AndroidManifest;
+
+mod dump;
+pub use dump::dump;
+
+pub fn compile(m: &AndroidManifest, w: &mut impl io::Write) -> anyhow::Result<()> {
+    Ok(())
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn manifest_based_on_czak_minimal_android_project() {
+        let m = android_manifest::from_str(MANIFEST_XML).unwrap();
+        let mut buf = Vec::<u8>::new();
+        compile(&m, &mut buf).unwrap();
+        assert_eq!(
+            MANIFEST_DUMP,
+            dump(&*buf).unwrap().join("\n"),
+        );
+    }
+
+    const MANIFEST_XML: &str = r#"<?xml version="1.0" encoding="utf-8"?>
+<manifest xmlns:android="http://schemas.android.com/apk/res/android"
+    package="com.akavel.hello">
+    <application android:label="HelloDali">
+        <activity android:name="HelloActivity">
+            <intent-filter>
+                <action android:name="android.intent.action.MAIN" />
+                <category android:name="android.intent.category.LAUNCHER" />
+            </intent-filter>
+        </activity>
+    </application>
+</manifest>
+"#;
+
+    const MANIFEST_DUMP: &str = r#"
+Binary XML
+N: android=http://schemas.android.com/apk/res/android
+  E: manifest
+    A: http://schemas.android.com/apk/res/android:compileSdkVersion(0x01010572)=28
+    A: http://schemas.android.com/apk/res/android:compileSdkVersionCodename(0x01010573)="9" (Raw: "9")
+    A: package="com.akavel.hello" (Raw: "com.akavel.hello")
+    A: platformBuildVersionCode=28 (Raw: "28")
+    A: platformBuildVersionName=9 (Raw: "9")
+      E: application
+        A: http://schemas.android.com/apk/res/android:label(0x01010001)="HelloDali" (Raw: "HelloDali")
+          E: activity
+            A: http://schemas.android.com/apk/res/android:name(0x01010003)="HelloActivity" (Raw: "HelloActivity")
+              E: intent-filter
+                  E: action
+                    A: http://schemas.android.com/apk/res/android:name(0x01010003)="android.intent.action.MAIN" (Raw: "android.intent.action.MAIN")
+                  E: category
+                    A: http://schemas.android.com/apk/res/android:name(0x01010003)="android.intent.category.LAUNCHER" (Raw: "android.intent.category.LAUNCHER")
+"#;
+}
